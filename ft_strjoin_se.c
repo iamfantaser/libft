8d1025bb442de5d6eb766a8a270b2c5e420e8e9f
Alexander Babiy <80643629+iamfantaser@users.noreply.github.com>
2022-01-26 16:52:20 +0300

    Update ft_strjoin_se.c

--- ft_strjoin_se.c
+++ ft_strjoin_se.c
@@ -19,6 +19,6 @@ char	*ft_strjoin_se(char *s1, char *s2)
 	else if (s2 == NULL && s1)
 		return (ft_strdup(s1));
 	else if (s1 && s2)
-		ft_strjoin(s1, s2);
+		return (ft_strjoin(s1, s2));
 	return (NULL);
 }
